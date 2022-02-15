790d2bb49ea7a9ad79f331185cd5d693595c1e81
pokemonsta433 <isaacwbg@gmail.com>
2022-02-15 13:01:41 -0500

    Refactored for spring boot
    If you're looking for assets, they're in  main/resources where all the other resources go. Now there's a spring boot thing that seems to work :)

---
+++ Quest/src/main/java/com/team17/quest/QuestApplication.java
@@ -0,0 +1,13 @@
+package com.team17.quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestApplication.class, args);
+    }
+
+}
---
+++ Quest/src/test/java/com/team17/quest/QuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team17.quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
