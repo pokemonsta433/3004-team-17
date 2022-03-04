13178daef97e1df8e0e450495be18027e0ea7d57
Noah Glogauer <noahglogauer@gmail.com>
2022-03-04 18:49:26 -0500

    Added inability to start with 1 player, added list of boost quests to foe cards

--- Quest/src/main/java/com/team17/quest/DefaultController.java
+++ Quest/src/main/java/com/team17/quest/DefaultController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.ArrayList;
